b71f28771983e22ecd3ede94378fbe19c1579187
Jeanlucascastro <61846534+Jeanlucascastro@users.noreply.github.com>
2020-07-30 21:57:17 -0300

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+
+  public static void main (String arg []){
+  
+  System.out.println ("Hello World");
+  System.out.println ("Ola mundo");
+  int y = 32;
+  System.out.println (y);
+  double z = 10.35784;
+  System.out.printf ("%.4f%n", z);
+
+  }
+}
