f47ac268448d3ff9ba9320707ccdf32905ea7149
Jeanlucascastro <61846534+Jeanlucascastro@users.noreply.github.com>
2020-07-30 22:12:23 -0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+import java.util.Locate;
+
 public class hello {
 
   public static void main (String arg []){
@@ -5,9 +7,15 @@ public class hello {
   System.out.println ("Hello World");
   System.out.println ("Ola mundo");
   int y = 32;
+  int a = 2;
   System.out.println (y);
   double z = 10.35784;
   System.out.printf ("%.4f%n", z);
+  System.out.printf("o reultado " + y + a + "metros");
+  System.out.printf (" Resultado =%.2f metros/5n", z);
+
+
+
 
   }
 }
