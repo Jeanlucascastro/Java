ad65c068bd6fe579200963ec45e12d57a35d8977
Jean Castro <61846534+Jeanlucascastro@users.noreply.github.com>
2020-08-27 20:14:08 -0300

    Update Bank.java

--- FixacaoM/entities/Bank.java
+++ FixacaoM/entities/Bank.java
@@ -1,33 +1,59 @@
 package entities;
-// comentando
-
+//comentando
 
 public class Bank {
 
-    public String holder;
-    public int number;
-    public double value;
+	private String holder;
+	private int number;
+	private double value;
 
-    public Bank(String holder,int number , double value){
-        this.holder = holder;
-        this.number = number;
-        this.value = value;
-    }
+	public Bank(String holder, int number, double value) {
+		this.holder = holder;
+		this.number = number;
+		this.value = value;
+	}
 
-    public double valorTotal () {
-        return value;
-    }
+	public double valorTotal() {
+		return value;
+	}
 
-    public void addValue (double value) {
-        this.value += value;
-    }
+	public void addValue(double value) {
+		this.value += value;
+	}
 
-    public void removeValue (double value) {
-        this.value -= value;
-    }
+	public void removeValue(double value) {
+		this.value -= (value * 0.5);
+	}
 
-    public String toString (){
-        return holder + " Conta " + number +  " Saldo " + value;
-    }
+	public String toString() {
+		return holder + " Conta " + number + " Saldo " + value;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+	
 
 }
